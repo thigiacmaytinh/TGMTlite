150f2164856c6563c3a5f64e5819d39ec58fa3dd
vohungvi <vohungvi@vohungvi.com>
2018-09-25 22:39:46 +0700

    - auto capture camera

--- TGMTtemplate/CameraExample.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class CameraExample
-{
-public:
-	static void main(int argc, char** argv);
-};
-
--- TGMTtemplate/WinMain.cpp
+++ TGMTtemplate/WinMain.cpp
@@ -3,11 +3,62 @@
 
 #include "stdafx.h"
 #include <tchar.h>
-#include "CameraExample.h"
+#include "TGMTcamera.h"
+#include "TGMTConfig.h"
+#include "TGMTdebugger.h"
+#include "TGMTutil.h"
+#include "TGMTfile.h"
+
+bool autoCapture = false;
+int interval;
+clock_t _time = clock();
+
+void OnKeyEvent(int key)
+{
+	if (key == KEY_ESC)
+	{
+		GetTGMTcamera()->StopCamera();
+	}
+	else if (key == KEY_ENTER)
+	{
+		autoCapture = !autoCapture;
+	}
+}
+
+void OnCameraFrames(std::vector<cv::Mat> frames)
+{
+	for (int i = 0; i < frames.size(); i++)
+	{		
+		if (autoCapture && clock() - _time > interval)
+		{
+			_time = clock();
+			WriteImage(frames[i], TGMTutil::FormatString("img\\%d.jpg", _time).c_str());
+		}
+		ShowImage(frames[i], "frame_%d", i);
+		OnKeyEvent(cv::waitKey(1));
+	}
+}
+
+void OnCameraStop()
+{
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	CameraExample::main(argc, argv);
+	SET_CONSOLE_TITLE("Auto capture camera");
+	GetTGMTConfig()->SetWriteDefaultValueIfNotExist(true);
+	GetTGMTConfig()->LoadSettingFromFile();
+	interval = GetTGMTConfig()->ReadValueInt("AutoCaptureCamera", "interval", 1000);
+
+	TGMTfile::CreateDir("img");
+	GetTGMTcamera()->OnNewFrames = OnCameraFrames;
+	GetTGMTcamera()->OnStopped = OnCameraStop;
+
+	GetTGMTcamera()->LoadConfig();
+
+
+
+	GetTGMTcamera()->Start();
 
 	cv::waitKey();
 	getchar();
